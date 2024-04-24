ee1e123287fcd77dececa9638cc307ed77d73e62
Len0ble <mansourba0109@gmail.com>
2024-04-25 00:22:24 +0200

    methode

--- 00_hello.rb
+++ 00_hello.rb
@@ -1,3 +1,18 @@
 def say_hello
   puts "Bonjour!"
 end
+# Appel de la methode
+say_hello
+def say_hello(first_name)
+  puts "Bonjour, #{first_name} !"
+end
+
+# Appel de la méthode
+say_hello("Mbacke")
+
+def ask_first_name
+  puts "Quel est votre prénom ?"
+  gets.chomp
+end
+
+
